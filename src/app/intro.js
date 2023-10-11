5a1d3f9cf89033168a1d6de47f58688451a92456
Franz Seidel <franzseidel@MacBook-Air-de-Franz.local>
2023-10-11 11:34:46 -0500

    pokédex

--- src/app/intro.js
+++ src/app/intro.js
@@ -11,16 +11,8 @@ const Home = () => {
   return (
 	<div className="text-center mt-5">
 		<h1>Database</h1>
-    <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h4 className="card-title">Squirtle</h4>
-    <h6 className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h6>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
         <p>
-			<Image  src="/img/squirtle.png" alt="Logo oficial " width={300}
+			<Image  src="/img/pokemon.png" alt="Logo oficial " width={600}
           height={300}
           priority />
       
