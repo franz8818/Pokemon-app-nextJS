04fb8cf8d0ddbeda9064ed3ca826c47ec67d9065
Franz Seidel <franzseidel@MacBook-Air-de-Franz.local>
2023-11-01 16:57:11 -0500

    estetica

--- src/app/page.js
+++ src/app/page.js
@@ -4,25 +4,32 @@ import {
 } from "@/components/bootstrap/client-bootstrap";
 
 export const metadata = {
-  title: process.env.APP_NAME +" | "+process.env.APP_DESCRIPTION,
+  title: process.env.APP_NAME + " | " + process.env.APP_DESCRIPTION,
 };
 
 const Home = () => {
   return (
-    <div className="container">
-	<div className="text-center">
-		<img src="/pokedex.svg" className="card-img-top" alt="..." />
-    <div className="card">
-    <Image  src="/img/squirtle.png" alt="Logo oficial " width={300}
+    <div className="container mt-5">
+      {/* <img src="/pokedex.svg" className="card-img-top" alt="..." /> */}
+      <div className="row" style={{ height: "50vh" }}>
+      <div className="col-12">
+      <div className="card" style={{ width: "19rem" }}>
+        <Image src="/img/squirtle.png" alt="Squirtle" width={300}
           height={300}
           priority />
-  <div className="card-body">
-    <h4 className="card-title">Squirtle</h4>
-    <h6 className="card-text">weight:</h6>
-  </div>
-  </div>
-  </div>
-</div>
+        <div className="card-body">
+        <div className="border p-3">
+          <h2 className="card-title">Squirtle </h2>
+          <h5 className="card-subtitle text-muted"><strong>ID</strong>  7 </h5>
+          <p className="card-text"><strong>Weight</strong> 9kg </p>
+          <p className="card-text"><strong>Height</strong> 0.5m </p>
+          <p className="card-text"><strong>Ability</strong> Torrent </p>
+        </div>
+        </div>
+      </div>
+      </div>
+    </div>
+    </div>
   );
 }
 
